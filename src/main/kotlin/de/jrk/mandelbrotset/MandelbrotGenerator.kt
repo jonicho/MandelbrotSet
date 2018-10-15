574a2d0a69e76478758c627fbf7977a98f5d4751
Jonas Keller <jonas.richardd@gmail.com>
2018-10-15 14:45:26 +0200

    generate mandelbrot set in batches

--- src/main/kotlin/de/jrk/mandelbrotset/MandelbrotGenerator.kt
+++ src/main/kotlin/de/jrk/mandelbrotset/MandelbrotGenerator.kt
@@ -1,8 +1,9 @@
 package de.jrk.mandelbrotset
 
-class MandelbrotGenerator(var numThreads: Int = 4) {
-    private val threads = emptyArray<Thread>()
-    var set = Array(1) {Array(1) {0} }
+import kotlin.math.absoluteValue
+
+class MandelbrotGenerator(val numThreads: Int = 4, val numBatchRows: Int = 16) {
+    var set = Array(1) { Array(1) { 0 } }
         private set
     var isGenerating = false
         private set
@@ -15,12 +16,53 @@ class MandelbrotGenerator(var numThreads: Int = 4) {
 
         Thread {
             isGenerating = true
-            val threads = Array(numThreads) {i ->
-                Thread {
-                    MandelbrotSet.generateMandelbrotSet(set, cX, cY, cWidth, cHeight, iterations, x = i * (set.size / numThreads), width = set.size / numThreads)
-                }.also { it.start() }
+            val spiralBatches = object {
+                //see: https://stackoverflow.com/a/1196922
+                var x = 0
+                var y = 0
+                var dx = 0
+                var dy = -1
+                var max = numBatchRows * numBatchRows
+                val halfNumBatchRows = numBatchRows / 2
+                var i = 0
+                val isFinished get() = synchronized(this) { i >= max }
+                fun nextBatch(): Pair<Int, Int> {
+                    synchronized(this) {
+                        i++
+                        if ((x.absoluteValue == y.absoluteValue && (dx != 1 || dy != 0)) || (x > 0 && y == 1 - x)) {
+                            val odx = dx
+                            dx = -dy
+                            dy = odx
+                        }
+                        if (x.absoluteValue > halfNumBatchRows || y.absoluteValue > halfNumBatchRows) {
+                            val odx = dx
+                            dx = -dy
+                            dy = odx
+                            val ox = x
+                            x = -y + dx
+                            y = ox + dy
+                        }
+                        val p = Pair(halfNumBatchRows - x, halfNumBatchRows - y)
+                        x += dx
+                        y += dy
+                        return p
+                    }
+                }
             }
-            for(thread in threads) {
+            val threads = Array(numThreads) { i ->
+                Thread {
+                    while (!spiralBatches.isFinished) {
+                        val (x, y) = spiralBatches.nextBatch()
+                        MandelbrotSet.generateMandelbrotSet(set, cX, cY, cWidth, cHeight, iterations,
+                                x = Math.round(x * set.size.toDouble() / numBatchRows).toInt(), width = Math.round(set.size.toDouble() / numBatchRows).toInt(),
+                                y = Math.round(y * set.size.toDouble() / numBatchRows).toInt(), height = Math.round(set.size.toDouble() / numBatchRows).toInt())
+                    }
+                }.also {
+                    it.priority = Thread.MIN_PRIORITY
+                    it.start()
+                }
+            }
+            for (thread in threads) {
                 thread.join()
             }
             isGenerating = false
