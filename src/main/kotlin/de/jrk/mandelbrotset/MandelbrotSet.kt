8f9d826f6b6be905763b432f8f5af50c41138ed8
Jonas Keller <jonas.richardd@gmail.com>
2018-10-20 20:23:34 +0200

    rename iterations to maxIterations

--- src/main/kotlin/de/jrk/mandelbrotset/MandelbrotSet.kt
+++ src/main/kotlin/de/jrk/mandelbrotset/MandelbrotSet.kt
@@ -1,13 +1,13 @@
 package de.jrk.mandelbrotset
 
 object MandelbrotSet {
-    fun testMandelbrot(c: Complex, iterations: Int): Int {
+    fun testMandelbrot(c: Complex, maxIterations: Int): Int {
         var z = c
         var i = 0
         do {
             if (z.squaredAbsoluteValue > 4) return i
             z = z * z + c
-        } while (++i < iterations)
+        } while (++i < maxIterations)
         return -1
     }
 }
